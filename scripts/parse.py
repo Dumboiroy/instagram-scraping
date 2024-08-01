21b2e5c88e4ea82321efa74f81162e9e5cd9b7d3
Dumboiroy <88130347+Dumboiroy@users.noreply.github.com>
2024-08-01 23:02:41 +0800

    asdasd

---
+++ scripts/parse.py
@@ -0,0 +1,50 @@
+comments_file = "data/comments.txt"
+
+with open(comments_file, "r", encoding="utf-8") as f:
+    comments = f.readlines()
+
+# skip first 25 lines
+comments = comments[25:]
+# skip last 10 lines
+comments = comments[:-10]
+
+# skipping view all replies
+comments_1 = []
+for line in comments:
+    if line.startswith("View all "):
+        # print("Skipping comment:", line)
+        continue
+    comments_1.append(line)
+
+# skipping Reply button
+skipcount = 0
+comments_2 = []
+for line in comments_1:
+    if skipcount > 0:
+        skipcount -= 1
+        # print("Skipping comment:", line)
+        continue
+    else:
+        if line.startswith("Reply\n"):
+            skipcount = 4
+            # print("Skipping comment:", line)
+            continue
+        else:
+            comments_2.append(line)
+
+# join 3 lines into 1
+comments_3 = []
+for i in range(0, len(comments_2), 3):
+    comments_3.append(comments_2[i] + comments_2[i + 1] + comments_2[i + 2])
+
+comments_4 = []
+for comment in comments_3:
+    # replace newline characters with | character
+    comment = comment.replace("\n", "|")
+    comments_4.append(comment)
+
+# write comments to file
+comments_file = "data/comments_cleaned.txt"
+with open(comments_file, "w", encoding="utf-8") as f:
+    for comment in comments_4:
+        f.write(comment + "\n")
---
+++ scripts/scrape.py
@@ -0,0 +1,98 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def scrape(email_login, password_login, post_url):
+
+    browser = webdriver.Firefox()
+    browser.implicitly_wait(5)
+
+    # Opening Instagram
+    try:
+        browser.get("https://www.instagram.com/accounts/login/?hl=en")
+        print("Opened Instagram.")
+
+        browser.implicitly_wait(5)
+
+        # login credentials
+        print("finding login credentials")
+        username_input = browser.find_element(By.NAME, "username")
+        password_input = browser.find_element(By.NAME, "password")
+
+        print("sending login credentials")
+        username_input.send_keys(email_login)
+        password_input.send_keys(password_login)
+
+        # Finding Login button
+        try:
+            login_link = browser.find_element(By.XPATH, "//button/div[text()='Log in']")
+        except Exception as e:
+            print("Error finding login button:", repr(e))
+
+        # Clicking on Login button
+        print("clicking login button")
+        try:
+            login_link.click()
+        except Exception as e:
+            print("Error clicking login button:", repr(e))
+
+        # Wait for 2FA to load
+        while True:
+            if "two_factor" not in browser.current_url:
+                print("waiting for 2FA to load...")
+                sleep(1)
+            else:
+                break
+
+        # 2FA
+        print(browser.current_url)
+        if "two_factor" in browser.current_url:
+            print("Two factor authentication required.")
+            while True:
+                # Await Two factor authentication
+                if "two_factor" not in browser.current_url:
+                    break
+                else:
+                    print("waiting...")
+                    sleep(1)
+        sleep(1)
+
+        # Opening Post
+        try:
+            post_url = post_url
+            browser.get(post_url)
+            print("Opened post.")
+        except Exception as e:
+            print("Error opening post:", repr(e))
+
+        # getting all comments
+        try:
+            comment_classes = "x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"
+            comments = browser.find_elements(
+                By.CSS_SELECTOR, f"div[class*='{comment_classes}']"
+            )
+            print("Found comments:", len(comments))
+            with open("data/comments.txt", "w", encoding="utf-8") as f:
+                print("Writing comments to file.")
+                for comment in comments:
+                    f.write(comment.text + "\n")
+                print("Comments written to file.")
+        except Exception as e:
+            print("Error finding comments:", repr(e))
+
+    except Exception as e:
+        print("Error opening Instagram:", repr(e))
+
+    browser.close()
+    print("Browser closed.")
+
+    quit()
+
+
+email = "alguitarmailbox@gmail.com"
+password = "Guitar123456"
+post_url = "https://www.instagram.com/p/C-HPL2mSrFV/"
+
+scrape(email, password, post_url)
